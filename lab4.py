798481ea667808b13587240603829e4bd6f9b0f1
Bc <lbx989@gmail.com>
2023-02-17 21:26:17 +0800

    python-telegram-bot升级为20.1

--- lab4.py
+++ lab4.py
@@ -1,5 +1,6 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext
+from telegram.ext import ApplicationBuilder, Updater, CommandHandler, MessageHandler, filters, CallbackContext, \
+    ContextTypes
 import configparser
 import logging
 import redis
@@ -9,20 +10,20 @@ def main():
 # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
-    dispatcher = updater.dispatcher
+
+
+    application = ApplicationBuilder().token("6248080117:AAH5nAixPYjbRl8zpCNE6R3dnEp9Yb1DeMc").build()
     global redis1
     redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
 
-    dispatcher.add_handler(CommandHandler("add", add))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("hello", hello))
-    updater.start_polling()
-    updater.idle()
+    application.add_handler(CommandHandler("add", add))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("hello", hello))
+    application.run_polling()
 
 
 def echo(update, context):
@@ -32,27 +33,27 @@ def echo(update, context):
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Helping you helping you.')
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Helping you helping you.')
 
 
-def hello(update: Update, context: CallbackContext) -> None:
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message is None:
         return
     user_input = update.message.text
     if user_input.startswith("/hello "):
-        update.message.reply_text("Good day, " + user_input[7:] + "!")
+        await update.message.reply_text("Good day, " + user_input[7:] + "!")
 
-def add(update: Update, context: CallbackContext) -> None:
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         global redis1
         logging.info(context.args[0])
         msg = context.args[0] # /add keyword <-- this should store the keyword
         redis1.incr(msg)
-        update.message.reply_text('You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
+        await update.message.reply_text(
+            'You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
     except (IndexError, ValueError):
-        update.message.reply_text('Usage: /add <keyword>')
-
+        await update.message.reply_text('Usage: /add <keyword>')
 
 if __name__ == '__main__':
     main()
